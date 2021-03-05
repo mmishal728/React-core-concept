215c1ee8f03fe21ce89251cc01cc2182a39479e3
mmishal728 <mmishal728@gmail.com>
2021-03-04 20:51:35 -0500

    core concept exposed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -9,17 +10,67 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter></Counter>
+        <Users></Users>
+        <p>My First React Paragraph</p>
+
+        <Person name="Shakib khan" nayeka="Bobli " ></Person>
+          <Person name="Jashim " nayeka="shabana" ></Person>
+            <Person name= "Rubel" nayeka="Mousumi"></Person>
+            <Person name = "Bapparas" nayeka="seka Khai"></Person>
       </header>
     </div>
   );
+}
+
+function Person(props) {
+  const PersonStyle = {
+    border:'2px solid cyan',
+    margin:'15px', 
+    borderRadius:'10px',
+    padding:'10px',
+    width:'500px',
+  }
+  return(
+  <div style= {PersonStyle}>
+      <h2>{props.name}</h2>
+      <h4>{props.nayeka}</h4>
+  </div>
+  )
+  
+};
+function Counter() {
+  const [count , setCount] = useState(10);
+  const handleIncrease = () =>setCount(count +1);
+  ;
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+       <button onClick={ () =>{setCount(count -1)} } >Decrease</button>
+      <button onClick={ () =>{setCount(count +1)} } >Increase</button>
+    </div>
+  )
+  
+}
+
+function Users() {
+  const [users, setUsers]= useState([]);
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => res.json())
+    .then(data=> setUsers(data))
+  }, [])
+  return(
+    <div>
+      <h1>Dynamic User :{users.length}</h1>
+      <ul>
+        {
+          users.map(user =><li>{user.name}, </li>)
+        }
+      </ul>
+    </div>
+  )
+  
 }
 
 export default App;
